f46d29afb13fca38712530e4f91439c82991f155
sergiobredor <sergiobredor.ing@gmail.com>
2026-01-19 17:39:05 -0500

    Initial commit: estructura base del proyecto de clases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { ListadoRegistrosComponent } from './pages/listado-registros/listado-registros.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    component: HomeComponent
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'perfil',
+    component: PerfilComponent
   },
+  {
+    path: 'galeria',
+    component: GaleriaComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
+  },
+  {
+    path: 'registros',
+    component: ListadoRegistrosComponent
+  }
 ];
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,18 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { importProvidersFrom } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(
+      IonicModule.forRoot(),
+      IonicStorageModule.forRoot()
+    )
   ],
 });
